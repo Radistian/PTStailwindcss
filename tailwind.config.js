14ab9877c0f8524d0e58e20e4834b0c977067382
Radistian <149747284+Radistian@users.noreply.github.com>
2024-03-25 10:02:16 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        df1:"#252B42",
+        df2:"#394264"
+      }
+    },
+  },
+  plugins: [],
+  darkMode:"class"
+}
